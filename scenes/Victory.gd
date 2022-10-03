c5e0fca18220650d40efa38a3848cacd909b53c4
damien <damiengabrielle@gmail.com>
2022-10-03 21:26:33 +0200

    fix victory

--- scenes/Victory.gd
+++ scenes/Victory.gd
@@ -3,9 +3,9 @@ extends Node2D
 func _ready() -> void:
 	$CanvasLayer/Fade.fade_in()
 	var text = "[center]You made it![/center]\n\n" \
-		+ "with [color=blue]{attempt}[/color] attempt(s)\n" \
-		+ "you pressed the buzzer [color=blue]{click}[/color] times(s)\n" \
-		+ "in [color=blue]{time}[/color] secondes"
+		+ "it took you [color=blue]{attempt}[/color] attempt(s)\n" \
+		+ "in [color=blue]{time}[/color] secondes\n\n" \
+		+ "you pressed the buzzer [color=blue]{click}[/color] times(s)\n"
 	var data = {
 		'attempt': Stats.nbr_attempt,
 		'click': Stats.nbr_click,
@@ -15,7 +15,7 @@ func _ready() -> void:
 	$CanvasLayer/Control/VBoxContainer/RichTextLabel.bbcode_text = text.format(data)
 
 func _on_Buzzer_press_finished() -> void:
-	Stats.nbr_attempt = 0
+	Stats.nbr_attempt = 1
 	Stats.nbr_click = 0
 	Stats.total_time = 0.0
 	$CanvasLayer/Fade.fade_out()
--- script/stats.gd
+++ script/stats.gd
@@ -1,5 +1,5 @@
 extends Node
 
 var total_time: float = 0.0
-var nbr_attempt: int = 0
+var nbr_attempt: int = 1
 var nbr_click: int = 0
